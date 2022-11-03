83930cba62a7b3ea6642883768de4f7446b23689
Brand27 <brandon.tardif.1@ulaval.ca>
2022-11-03 14:47:33 -0400

    debut main menu

---
+++ Assets/MainMenu/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
